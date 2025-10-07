d6c990f5e6b6f084913e2af292d4da8f8687a3d6
camnhungngo23-creator <camnhungngo23@gmail.com>
2025-10-07 09:42:03 +0700

    Update options.tsx
    Signed-off-by: camnhungngo23-creator <camnhungngo23@gmail.com>

--- components/auto-complete/demo/options.tsx
+++ components/auto-complete/demo/options.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
       if (!value || value.includes('@')) {
         return [];
       }
-      return ['gmail.com', '163.com', 'qq.com'].map((domain) => ({
+      return ['Ang Mo Kio', '163.com', 'qq.com'].map((domain) => ({
         label: `${value}@${domain}`,
         value: `${value}@${domain}`,
       }));
